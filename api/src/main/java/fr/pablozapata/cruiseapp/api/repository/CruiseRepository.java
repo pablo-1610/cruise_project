95c4074b3ce3c4c066ae4d290f2881d516696364
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-03-08 10:38:14 +0100

    Remove current API

--- api/src/main/java/fr/pablozapata/cruiseapp/api/repository/CruiseRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.pablozapata.cruiseapp.api.repository;
-
-import fr.pablozapata.cruiseapp.api.model.CruiseModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CruiseRepository extends JpaRepository<CruiseModel, Long> {
-}
